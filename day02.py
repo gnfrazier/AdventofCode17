d17f9e642b7214afa629af83950e43bcdf39bc08
Greg <gnfrazier@gmail.com>
2017-12-02 08:28:39 -0500

    Add comments and notation.

--- day02.py
+++ day02.py
@@ -5,7 +5,9 @@ rows = [[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]
 
 checksum = 0
 for line in rows:
+    # Find the difference largest and smallest value in a line
     diff = max(line) - min(line)
+    # Add that difference to the total
     checksum = checksum + diff
 
 print(checksum)
@@ -19,15 +21,18 @@ rows = [[5, 9, 2, 8],
 checksum = 0
 
 for line in rows:
+    # Enumerate the numbers on a line
     eline = list(enumerate(line))
 
+    # Iterate through the numbers as denominators
     for number in eline:
+
         denominator = number
 
         for numerator in eline:
-
+            # Don't divide by the same position number
             if numerator[0] != denominator[0]:
-
+                # Check the modulo, if zero then add quotient to total
                 if numerator[1] % denominator[1] == 0:
                     checksum = checksum + (numerator[1] / denominator[1])
 
