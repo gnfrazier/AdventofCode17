77e36373deb11b140ad6dd89a24110a6adcf4a79
Greg <gnfrazier@gmail.com>
2017-12-03 21:34:25 -0500

    Day 3 commit.

---
+++ day03.py
@@ -0,0 +1,30 @@
+'''Advent of Code Day 2'''
+
+# Part One
+'''Needed hints and clues from message boards, started to
+use the length  of Archimedes Spiral but got stuck'''
+
+import math
+
+n = 1024
+
+# Find the layer by determining the closest odd square
+
+layer = math.ceil(math.sqrt(n))
+
+if layer % 2 != 0:
+    pass
+else:
+    layer = layer + 1
+
+# Radial distance
+rdist = (layer - 1) / 2
+
+# Find the shortest distance to path
+path = []
+for i in range(0, 4):
+    path.append(layer**2 - ((layer - 1) * i) - math.floor(layer / 2))
+pdist = []
+for i in path:
+    pdist.append(abs(i - n))
+print(min(pdist) + rdist)
