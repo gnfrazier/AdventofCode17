60948e5500fb7d0ea873ec2f84bec3532edb8a72
Greg <gnfrazier@gmail.com>
2017-12-05 17:44:49 -0500

    Solution for day 5 part two.

--- day05.py
+++ day05.py
@@ -35,3 +35,31 @@ while current <= escape:
     cycles = cycles + 1
 print(inst)
 print(escape, current, cycles)
+
+# Part Two
+# Same as part 1 with an added if to increase or decrease
+
+inst = list(enumerate(start))
+last = inst[-1]
+escape = last[0]
+
+
+def cycle(pair):
+    '''Added conditional for increase or decrease for part two'''
+    if pair[1] >= 3:
+        offset = (pair[0], pair[1] - 1)
+    else:
+        offset = (pair[0], pair[1] + 1)
+    return offset
+
+
+cycles = 0
+current = 0
+while current <= escape:
+    jump = inst[current]
+    next_step = current + jump[1]
+    inst[current] = cycle(jump)
+    current = next_step
+    cycles = cycles + 1
+
+print(cycles)
