832a744c7a7f6401df6bc588cf95e2063a4f970a
Greg <gnfrazier@gmail.com>
2017-12-11 12:57:39 -0500

    Solution Day 10 Part One.

--- day10.py
+++ day10.py
@@ -2,54 +2,55 @@
 
 # Part One
 
-# l engths = [63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24]
-lengths = [3, 4, 1, 5]
-# Generate list 0 to 255
-# input = []
-input = [0, 1, 2, 3, 4, ]
+lengths = [63, 144, 180, 149, 1, 255, 167,
+           84, 125, 65, 188, 0, 2, 254, 229, 24]
+
+input = []
+
+for i in range(0, 256):
+    input.append(i)
 
 current_position = 0
 skip_size = 0
 last = len(input) - 1
-'''for i in range(0,256):
-    input.append(i)'''
+
 
 for i in lengths:
-    # i = 3
-    '''Reverse the order of that length of elements in the list,
-    starting with the element at the current position.'''
 
     section = []
     read_position = current_position
     for step in range(i):
-        print(current_position, read_position)
+
         section.append(input[read_position])
         read_position += 1
-        if read_position >= last:
+        if read_position > last:
 
             read_position = 0
-            print('wrap')
+
     section.reverse()
     print(section)
     write_position = current_position
 
     for item in section:
-        # print(input, write_position, item)
+
         input[write_position] = item
 
         write_position += 1
 
-        if write_position == (len(input) - 1):
+        if write_position > last:
             write_position = 0
 
     '''Move the current position forward by that length plus the skip size.'''
-    if current_position + i + skip_size < last:
-        current_position = current_position + i + skip_size
+    print(current_position, i, skip_size, last)
+    if current_position + i + skip_size > last:
+        current_position = current_position + i + skip_size - last - 1
     else:
-        current_position = current_position + i + skip_size - last
+        current_position = current_position + i + skip_size
 
-    '''Increase the skip size by one.'''
-    skip_size = + 1
-    # print(section)
+    print(current_position, i, skip_size)
+
+    skip_size += 1
+
     print(input, 'end')
-    # print(current_position, skip_size)
+
+print(input[0] * input[1])
