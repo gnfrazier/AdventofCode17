baabf7cf9941c38ea7e66d147fee59ec3d4b52bd
Greg <gnfrazier@gmail.com>
2017-12-18 21:12:04 -0500

    Solution Part Two

--- day17.py
+++ day17.py
@@ -1,7 +1,7 @@
 
 '''Advent of Code Day 17'''
 
-
+# Part One
 from collections import deque
 step = 369
 # step = 3 # Sample input
@@ -20,3 +20,23 @@ for i in range(1, 2018):
 
 
 print(d[-1])
+
+# Part Two
+
+step = 369
+# step = 3 # Sample input
+d = deque('0')
+
+
+def insert(sd, rotation, new_value):
+
+    sd.rotate(rotation)
+    sd.appendleft(new_value)
+    return sd
+
+
+for i in range(1, 50000000):
+    d = insert(d, step, i)
+
+
+print(d[(d.index('0') - 1)])
